e957f9cb1e1f00f31752c9bfd6e6c5547b59b327
Ben Woodard <woodard@redhat.com>
2026-08-27 23:47:21 -0700

    Change the frequency to MHz
    Signed-off-by: Ben Woodard <woodard@redhat.com>

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 void print_usage(const char* prog_name) {
     std::cout << "Usage: " << prog_name << " [options]\n"
               << "Options:\n"
-              << "  -f, --freq <Hz>       Frequency to set in Hz (e.g., 144390000)\n"
+              << "  -f, --freq <MHz>      Frequency to set in MHz (e.g., 144.390)\n"
               << "  -p, --port <device>   Serial port (default: /dev/ttyUSB0)\n"
               << "  -m, --model <id>      Hamlib rig model ID (default: 2 for generic Kenwood)\n"
               << "  -h, --help            Show this help message\n";
@@ -15,7 +15,8 @@ void print_usage(const char* prog_name) {
 
 int main(int argc, char* argv[]) {
     // Default parameters
-    freq_t target_freq = 0;
+    double target_freq_mhz = 0.0;
+    freq_t target_freq_hz = 0;
     std::string serial_port = "/dev/ttyUSB0";
     rig_model_t rig_model = RIG_MODEL_KENWOOD; // Model 2 (Generic Kenwood)
 
@@ -33,7 +34,8 @@ int main(int argc, char* argv[]) {
     while ((opt = getopt_long(argc, argv, short_opts, long_opts, nullptr)) != -1) {
         switch (opt) {
             case 'f':
-                target_freq = std::stod(optarg);
+                target_freq_mhz = std::stod(optarg);
+                target_freq_hz = static_cast<freq_t>(target_freq_mhz * 1000000.0);
                 break;
             case 'p':
                 serial_port = optarg;
@@ -50,8 +52,8 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if (target_freq == 0) {
-        std::cerr << "Error: You must specify a target frequency.\n";
+    if (target_freq_hz == 0) {
+        std::cerr << "Error: You must specify a target frequency in MHz.\n";
         print_usage(argv[0]);
         return 1;
     }
@@ -67,9 +69,6 @@ int main(int argc, char* argv[]) {
     // Configure the serial port
     strncpy(my_rig->state.rigport.pathname, serial_port.c_str(), FILPATHLEN - 1);
     
-    // Set debug level (optional, helpful for troubleshooting)
-    // rig_set_debug(RIG_DEBUG_ERR);
-
     // Open the connection to the radio
     std::cout << "Connecting to radio on " << serial_port << "...\n";
     if (rig_open(my_rig) != RIG_OK) {
@@ -79,8 +78,8 @@ int main(int argc, char* argv[]) {
     }
 
     // Set the frequency on the current active VFO
-    std::cout << "Setting frequency to " << target_freq / 1e6 << " MHz...\n";
-    int status = rig_set_freq(my_rig, RIG_VFO_CURR, target_freq);
+    std::cout << "Setting frequency to " << target_freq_mhz << " MHz...\n";
+    int status = rig_set_freq(my_rig, RIG_VFO_CURR, target_freq_hz);
     if (status != RIG_OK) {
         std::cerr << "Error: Failed to set frequency. Hamlib error code: " << status << "\n";
     } else {
