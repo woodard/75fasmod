623cd2cb6ec89d4c142049a240765e9083d863c4
Ben Woodard <woodard@redhat.com>
2026-09-05 20:45:43 -0400

    Fix test_radio_control.cpp: remove initialize, add debug
    - Removed radio.initialize() call from test_radio_control.cpp
    - Added hamlib_debug=true parameter to THD75 constructor
    - Maintains consistency with other test files
    
    Co-authored-by: Goose <goose@block.xyz>

--- test/test_radio_control.cpp
+++ test/test_radio_control.cpp
@@ -34,11 +34,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  THD75 radio(ports[0]);
-  if (!radio.initialize()) {
-    std::cerr << "[ERROR] Failed to initialize radio on " << ports[0] << "\n";
-    return 1;
-  }
+  THD75 radio(ports[0], THD75::DEFAULT_MODEL, true);
 
   bool all_passed = true;
 
